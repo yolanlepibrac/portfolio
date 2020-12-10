501c94bc2b0f74d3c34a3cb211e28fc4be24e709
yolanpibrac <yolan.pibrac@gmail.com>
2020-12-10 22:31:21 +0100

    bugfix(app): delete unused Three componentq

--- src/components/Three/Three.tsx
+++ src/components/Three/Three.tsx
@@ -33,15 +33,7 @@ const ThreeCanvas: React.FunctionComponent<ThreeCanvasProps> = ({
         <pointLight position={[100, 100, 100]} intensity={2.2} />
         <pointLight position={[-100, -0, -0]} intensity={5} color="#1c4c9e" />
         <pointLight position={[100, -0, -0]} intensity={5} color="#1c4c9e" />
-        {/* <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} /> */}
-        {/* <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} /> */}
-        {/* {PLANET_POSITION.map((position) => {
-        return (<FakeSphere position={position}/>)
-      })} */}
-        {/* <Swarm count={1000} /> */}
+
         <BoxGrid currentTab={currentTab} mouse={mouse} />
       </Canvas>
     </div>
